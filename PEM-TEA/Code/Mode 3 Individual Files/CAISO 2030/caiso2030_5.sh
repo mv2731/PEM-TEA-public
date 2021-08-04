bce2ca55321a98a6773fd9efca971904d939af3d
Maya Venkatraman <mvenkat@google.com>
2021-08-04 07:46:10 -0400

    main: Add all data and code files to repo

---
+++ PEM-TEA/Code/Mode 3 Individual Files/CAISO 2030/caiso2030_5.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Simple "caiso2030_5" submit script for Slurm.
+#
+#SBATCH --account=free    # The account name for the job.
+#SBATCH --job-name=caiso2030_5 # The job name.
+#SBATCH -c 1 # The number of cpu cores to use.
+#SBATCH --time=360:00 # The time the job will take to run.
+#SBATCH --mem-per-cpu=5gb # The memory the job will use per cpu core.
+ 
+module load anaconda
+ 
+#Command to execute Python program
+python caiso2030_5.py
+ 
+#End of script
